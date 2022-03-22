fe7bfae2703fe9e95070bfebface9a1e12afc395
Yohta Kimura <kitakita7617@gmail.com>
2022-03-22 22:47:44 +0900

    add failing test case

---
+++ tests/PHPStan/Analyser/data/bug-6870.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1); // lint >= 8.0
+
+use function PHPStan\Testing\assertType;
+
+class HelloWorld
+{
+	public function foo(?string $data): void
+	{
+		$data === null ? throw new \Exception() : $data;
+		assertType('string', $data);
+	}
+
+	public function bar(?string $data): void
+	{
+		$data || throw new \Exception();
+		assertType('non-empty-string', $data);
+	}
+
+	public function baz(?string $data): void
+	{
+		!$data && throw new \Exception();
+		assertType('non-empty-string', $data);
+	}
+
+	public function boo(?string $data): void
+	{
+		$data ?? throw new \Exception();
+		assertType('string', $data);
+	}
+}
